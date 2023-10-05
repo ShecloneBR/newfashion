b7bceb2a83fe1c2f1faf3f0bed7fde2ad133d89f
Neverton <131413133+ShecloneBR@users.noreply.github.com>
2023-10-05 11:23:00 -0300

    log 3 la famme fashion

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,16 @@ const app = express()
 app.get("/", function(req, res){
     res.sendFile("/workspaces/newfashion/index.html")
 })
+app.get("todos", function(req, res){
+    res.sendFile("/../frontend/todos.html")
+})
+
+app.get("promocao", function(req, res){
+    res.sendFile("/../frontend/promocao.html")
+})
+
+app.get("carrinho", function(req, res){
+    res.sendFile("/../frontend/carrinho.html")
+})
 
 app.listen(14780, function(){ console.log ("servidor rodando na porta 14780.")})
