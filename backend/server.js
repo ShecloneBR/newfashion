d8a04d5914ef867eec2fdf61f597d65ecb902c83
Neverton <131413133+ShecloneBR@users.noreply.github.com>
2023-10-10 11:33:16 +0000

    log 6 new fashion

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,24 @@ const express = require('express')
 const app = express()
 
 app.get("/", function(req, res){
-    res.sendFile("/workspaces/newfashion/index.html")
+    res.sendFile("/../frontend/index.html")
 })
-app.get("todos", function(req, res){
+app.get("/todos", function(req, res){
     res.sendFile("/../frontend/todos.html")
 })
 
-app.get("promocao", function(req, res){
+app.get("/promocao", function(req, res){
     res.sendFile("/../frontend/promocao.html")
 })
 
-app.get("carrinho", function(req, res){
+app.get("/carrinho", function(req, res){
     res.sendFile("/../frontend/carrinho.html")
 })
+app.get("/login", function(req, res){
+    res.sendFile("/../frontend/login.html")
+})
+app.get("/cadrastar", function(req, res){
+    res.sendFile("/../frontend/cadrastar.html")
+})
 
-app.listen(14780, function(){ console.log ("servidor rodando na porta 14780.")})
+app.listen(14780, function() { console.log ("servidor rodando na porta 14780.") })
